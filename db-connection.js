6a02c09924497afa2e5c0191f22e4f35c5052f15
Mohammed Essehemy <mohammedessehemy@gmail.com>
2021-01-25 13:50:55 +0200

    feat: mongo env variable

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/iti-25-jan-2021', (err) => {
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/iti-25-jan-2021', (err) => {
     if(err) {
         console.warn("failed to connect to MongoDB")
         console.error(err);
